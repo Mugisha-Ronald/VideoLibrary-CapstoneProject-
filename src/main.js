4b9b1f9a805d85d9ab793ef10654dc8e900a41ae
Mugisha-Ronald <mugishacoder@gmail.com>
2022-04-19 18:11:36 +0300

    added signup/ login logic

--- src/main.js
+++ src/main.js
@@ -1,56 +1,52 @@
+// Registration
+
+let emailInput = document.querySelector('#email');
+
+let pswdInput = document.querySelector('#psw');
+
+let passwdReptInpt = document.querySelector('#psw-repeat');
+
+let theForm = document.querySelector('form');
 
 
-function setFormMessage(formElement, type, message) {
-    const messageElement = formElement.querySelector(".form__message");
 
-    messageElement.textContent = message;
-    messageElement.classList.remove("form__message--success", "form__message--error");
-    messageElement.classList.add(`form__message--${type}`);
-}
 
-function setInputError(inputElement, message) {
-    inputElement.classList.add("form__input--error");
-    inputElement.parentElement.querySelector(".form__input-error-message").textContent = message;
-}
 
-function clearInputError(inputElement) {
-    inputElement.classList.remove("form__input--error");
-    inputElement.parentElement.querySelector(".form__input-error-message").textContent = "";
-}
+theForm.addEventListener('submit', function(){
 
-document.addEventListener("DOMContentLoaded", () => {
-    const loginForm = document.querySelector("#login");
-    const createAccountForm = document.querySelector("#createAccount");
+    let usersData = [
 
-    document.querySelector("#linkCreateAccount").addEventListener("click", e => {
-        e.preventDefault();
-        loginForm.classList.add("form--hidden");
-        createAccountForm.classList.remove("form--hidden");
-    });
+        
+    {
+        'email': emailInput.value,
+        'password': pswdInput.value,
+        'repeat_psswrd':passwdReptInpt.value,
 
-    document.querySelector("#linkLogin").addEventListener("click", e => {
-        e.preventDefault();
-        loginForm.classList.remove("form--hidden");
-        createAccountForm.classList.add("form--hidden");
-    });
+    }
+]
 
-    loginForm.addEventListener("submit", e => {
-        e.preventDefault();
-       
-        window.open('./home.html')
+    let userData = JSON.parse(localStorage.getItem('usersData'));
 
-        setFormMessage(loginForm, "error", "Invalid username/password combination");
-    });
+    if(userData){
 
-    document.querySelectorAll(".form__input").forEach(inputElement => {
-        inputElement.addEventListener("blur", e => {
-            if (e.target.id === "signupUsername" && e.target.value.length > 0 && e.target.value.length < 10) {
-                setInputError(inputElement, "Username must be at least 10 characters in length");
-            }
-        });
+        localStorage.setItem('usersData',JSON.stringify([ ...usersData, ...userData]));
+    }else{
 
-        inputElement.addEventListener("input", e => {
-            clearInputError(inputElement);
-        });
-    });
+        localStorage.setItem('usersData', JSON.stringify(usersData));
+
+    }
+
+
+
+
+    alert('Registration successful, you can now login')
+    theForm.reset()
+    
+    window.open('/index.html');
+
+    
 });
+
+
+
+
